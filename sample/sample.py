8f9a539236f5b721a2467b84e8c546a25ba2e953
Jin <lsjhome007@gmail.com>
2019-04-15 10:19:33 +0900

    Create sample.py

---
+++ sample/sample.py
@@ -0,0 +1,21 @@
+class CountCharacter(object):
+
+    def __init__(self, input_words=None):
+        """
+        Args:
+            input_word(str): given words
+        """
+        self.input_words = input_words
+        
+    def count(self, character):
+        """
+        Count the number of character we look for in given words
+        Args:
+            character(str): a character look for
+
+        Return(int): the number of character
+
+        """
+        c_list = [i for i in self.input_words]
+        n = sum([True for char in c_list if char == character])
+        return n
