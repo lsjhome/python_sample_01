82eb42c810505816832f99f8e97266e210f2b31d
lsjhome <lsjhome007@gmail.com>
2019-04-15 10:30:55 +0900

    add example at count method

--- utils/sample.py
+++ utils/sample.py
@@ -10,11 +10,16 @@ class CountCharacter(object):
     def count(self, character):
         """
         Count the number of character we look for in given words
+        
         Args:
             character(str): a character look for
 
         Return(int): the number of character
-
+        
+        Example:
+            >>> cc = CountCharacter("Hello World")
+            >>> cc.Count('l')
+            3
         """
         c_list = [i for i in self.input_words]
         n = sum([True for char in c_list if char == character])
