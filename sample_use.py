f0e0733c401aee646ff0a868ffc3b83588473fa3
lsjhome <lsjhome007@gmail.com>
2019-04-15 10:37:48 +0900

    add some words for input

--- sample_use.py
+++ sample_use.py
@@ -2,6 +2,6 @@ from utils.sample import CountCharacter
 
 if __name__ == '__main__':
 
-    cc = CountCharacter(input())
-    n_count = cc.count(input())
+    cc = CountCharacter(input("Input words: "))
+    n_count = cc.count(input("Input a character: "))
     print (n_count)
